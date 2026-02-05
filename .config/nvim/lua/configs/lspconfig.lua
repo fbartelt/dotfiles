d6672b2b71e148a2ab11984c4109b9920aa989df
fbartelt <fbarteltp@gmail.com>
2026-02-04 21:05:02 -0300

    fix: change lspconfig to new syntax

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -7,35 +7,70 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "pylsp", "clangd" } -- add more servers here
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+-- base config shared by all servers
+local base = {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+
+-- servers with default config
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, base)
+  vim.lsp.enable(server)
 end
 
-lspconfig.pylsp.setup({
-    settings = {
-        pylsp = {
-            plugins = {
-                black = {
-                    enabled = true,  -- Enable Black formatter
-                    --line_length = 88 --
-                },
-            },
+-- pylsp with extra settings
+vim.lsp.config("pylsp", vim.tbl_deep_extend("force", base, {
+  settings = {
+    pylsp = {
+      plugins = {
+        black = {
+          enabled = true, -- Enable Black formatter
         },
+      },
     },
-})
+  },
+}))
+vim.lsp.enable("pylsp")
 
-lspconfig.clangd.setup {
-    on_attach = function(client, bufnr)
-        client.server_capabilities.signatureHelpProvider = false
-        nvlsp.on_attach(client, bufnr)
-    end,
-    capabilities = nvlsp.capabilities,
-}
+-- clangd with override
+vim.lsp.config("clangd", vim.tbl_deep_extend("force", base, {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    nvlsp.on_attach(client, bufnr)
+  end,
+}))
+vim.lsp.enable("clangd")
+-- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
+--
+-- lspconfig.pylsp.setup({
+--     settings = {
+--         pylsp = {
+--             plugins = {
+--                 black = {
+--                     enabled = true,  -- Enable Black formatter
+--                     --line_length = 88 --
+--                 },
+--             },
+--         },
+--     },
+-- })
+--
+-- lspconfig.clangd.setup {
+--     on_attach = function(client, bufnr)
+--         client.server_capabilities.signatureHelpProvider = false
+--         nvlsp.on_attach(client, bufnr)
+--     end,
+--     capabilities = nvlsp.capabilities,
+-- }
 -- lspconfig.pyright.setup({
     -- on_attach = nvlsp.on_attach,
     -- capabilities = nvlsp.capabilities,
